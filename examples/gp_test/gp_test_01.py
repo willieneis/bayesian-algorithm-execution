876f52ca83124e27a383405cf2d771dacc881036
willieneis <willie.neiswanger@gmail.com>
2020-11-14 19:59:51 -0800

    examples/gp_test: add two example scripts

--- examples/gp_test/gp_test_01.py
+++ examples/gp_test/gp_test_01.py
@@ -3,34 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from bax.models.simple_gp import SimpleGp
+from bax.models.exe_path import ExePath
 
 #import neatplot
 
 seed = 15
 np.random.seed(seed)
 
-# Instantiate GP
+# Instantiate GP with data
 gp = SimpleGp({'ls': 2.0, 'alpha': 1.5})
-
-# Set data
 data = {'x': [[1.0], [2.0], [3.0]], 'y': [-0.5, 0.0, 0.5]}
+gp.set_data(data)
 
-# Predefine algorithm execution x-path
+# Predefine execution x-path
 x_path = [[x] for x in np.linspace(3.5, 20, 100)]
 
 def sample_exe_path():
-    """ Return execution path sample. """
-    exe_path_sample = copy.deepcopy(data)
-
+    """Return execution path sample."""
+    ep = ExePath(verbose=False)
+    ep.init_path_with_model(gp)
     for x in x_path:
-        gp.set_data(exe_path_sample)
-        y = gp.sample_post_pred(x, 1)
-        y = y[0]
+        y = ep.next_step(x)
 
-        exe_path_sample['x'].append(x)
-        exe_path_sample['y'].append(y)
-
-    return exe_path_sample
+    return ep.exe_path
 
 
 # Define "true execution path"
@@ -46,10 +41,11 @@ for _ in range(n_paths):
 
     # Plot execution path sample
     n_data = len(data['x'])
-    plt.plot(exe_path_sample['x'][n_data-1:], exe_path_sample['y'][n_data-1:], 'o--')
+    plt.plot(exe_path_sample.x[n_data-1:], exe_path_sample.y[n_data-1:], 'o--')
 
-# Plot true execution path
-plt.plot(exe_path_true['x'], exe_path_true['y'], 'o--', color='k')
+# Plot data and true execution path
+plt.plot(data['x'], data['y'], 'o--', color='k')
+plt.plot(exe_path_true.x, exe_path_true.y, 'o--', color='k')
 
 plt.xlim([0, 21])
 plt.ylim([-4, 4])
