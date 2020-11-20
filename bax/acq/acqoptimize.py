45fb05c5051d1998ee6ef9610c0aafd4d10bb490
willieneis <willie.neiswanger@gmail.com>
2020-11-20 03:46:21 -0800

    reorganize Acqoptimizer viz/plotting methods, add plot post f samples

--- bax/acq/acqoptimize.py
+++ bax/acq/acqoptimize.py
@@ -82,7 +82,7 @@ class AcqOptimizer:
 
         # Optionally: visualize acqoptimizer
         if self.params.viz_acq:
-            self.viz_acqoptimizer(
+            self.plot_acqoptimizer_all(
                 model, exe_path_list, acq_list, x_test, mu, std, mu_list, std_list
             )
 
@@ -97,29 +97,42 @@ class AcqOptimizer:
         exe_path, _ = algo.run_algorithm_on_f(fs)
         return exe_path
 
-    def viz_acqoptimizer(
+    def plot_acqoptimizer_all(
         self, model, exe_path_list, acq_list, x_test, mu, std, mu_list, std_list
     ):
         """
         Visualize the acquisition function, optimization, and related details, for a 1D
         continuous domain.
         """
+        # Plot various details
+        h0 = self.plot_exe_path_samples(exe_path_list)
+        h1 = self.plot_postpred(x_test, mu, std)
+        #h1b = self.plot_post_f_samples(x_test, mu_list)
+        h2 = self.plot_postpred_given_exe_path_samples(x_test, mu_list, std_list)
+        h3 = self.plot_acqfunction(x_test, acq_list)
+        h4 = self.plot_model_data(model)
 
-        # Plot execution path samples
+        ## Legend
+        plt.legend(handles=[h0[0], h1, h2, h3[0], h4[0]], loc=1)
+
+    def plot_exe_path_samples(self, exe_path_list):
+        """Plot execution path samples."""
         for exe_path in exe_path_list:
-            h0 = plt.plot(
-                    exe_path.x,
-                    exe_path.y,
-                    '.',
-                    markersize=4,
-                    linewidth=0.5,
-                    label='$\{ \\tilde{e}_\mathcal{A}^j \} \sim p(e_\mathcal{A}(f) | \mathcal{D}_t)$',
+            h = plt.plot(
+                exe_path.x,
+                exe_path.y,
+                '.',
+                markersize=4,
+                linewidth=0.5,
+                label='$\{ \\tilde{e}_\mathcal{A}^j \} \sim p(e_\mathcal{A}(f) | \mathcal{D}_t)$',
             )
+        return h
 
-        # Plot posterior predictive
+    def plot_postpred(self, x_test, mu, std):
+        """Plot posterior predictive distribution."""
         lcb = mu - 3 * std
         ucb = mu + 3 * std
-        h1 = plt.fill_between(
+        h = plt.fill_between(
             np.array(x_test).reshape(-1),
             lcb,
             ucb,
@@ -127,12 +140,14 @@ class AcqOptimizer:
             alpha=0.2,
             label='$p(y|\mathcal{D}_t, x)$',
         )
+        return h
 
-        # Plot posterior predictive given execution paths
+    def plot_postpred_given_exe_path_samples(self, x_test, mu_list, std_list):
+        """Plot posterior predictive given execution path sample, for each sample."""
         for mu_samp, std_samp in zip(mu_list, std_list):
             lcb = mu_samp - 3 * std_samp
             ucb = mu_samp + 3 * std_samp
-            h2 = plt.fill_between(
+            h = plt.fill_between(
                 np.array(x_test).reshape(-1),
                 lcb,
                 ucb,
@@ -140,11 +155,37 @@ class AcqOptimizer:
                 alpha=0.1,
                 label='$p(y|\mathcal{D}_t, \\tilde{e}_\mathcal{A}, x)$',
             )
+        return h
 
-        # Plot data
-        plt.plot(model.data.x, model.data.y, 'o', color='deeppink')
+    def plot_post_f_samples(self, x_test, mu_list):
+        """Plot posterior function samples."""
+        # TODO: consider whether following are true posterior samples.
+        for mu_samp in mu_list:
+            h = plt.plot(
+                np.array(x_test).reshape(-1),
+                mu_samp,
+                '-',
+                alpha=0.75,
+                linewidth=0.5,
+                label='$\{\\tilde{f}\} \sim p(f | \mathcal{D}_t)$',
+            )
+        return h
 
-        # Plot acquisition function
+    def plot_model_data(self, model):
+        """Plot model.data."""
+        h = plt.plot(
+            model.data.x, model.data.y, 'o', color='deeppink', label='Observations'
+        )
+        # -----
+        #plt.plot([0, 20], [0,0], '--', color='k', linewidth=0.5)
+        #for x, y in zip(model.data.x, model.data.y):
+            #plt.plot([x, x], [0, y], '-', color='b', linewidth=0.5)
+        #h = plt.plot(model.data.x, model.data.y, 'o', color='b')
+        # -----
+        return h
+
+    def plot_acqfunction(self, x_test, acq_list):
+        """Plot acquisition function and dividing line, and resize plot axes."""
         ylim = plt.gca().get_ylim()
         acq_arr = np.array(acq_list)
         min_acq = np.min(acq_arr)
@@ -153,7 +194,7 @@ class AcqOptimizer:
         acq_height = 0.33 * ylim_diff
         ylim_new_min = ylim[0] - acq_height
         acq_arr = (acq_arr - min_acq) / (max_acq - min_acq) * acq_height + ylim_new_min
-        h3 = plt.plot(
+        h = plt.plot(
             np.array(x_test).reshape(-1),
             acq_arr,
             '-',
@@ -169,8 +210,7 @@ class AcqOptimizer:
         xlim = plt.gca().get_xlim()
         plt.plot(xlim, [ylim[0], ylim[0]], '--', color='k')
 
-        # Legend
-        plt.legend(handles=[h0[0], h1, h2, h3[0]], loc=1)
+        return h
 
     def print_str(self):
         """Print a description string."""
