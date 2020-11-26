38de7d14ac0f89290e0e4a686dd74fa6ccfccc7c
Alex Wang <kaw293@cornell.edu>
2020-11-25 19:51:26 -0800

    Separate out Algorithms base class

--- bax/alg/algorithms.py
+++ bax/alg/algorithms.py
@@ -5,16 +5,13 @@ Algorithms for BAX.
 from argparse import Namespace
 import copy
 import numpy as np
+from abc import ABC, abstractmethod
 
-from ..models.exe_path import ExePath
 from ..util.misc_util import dict_to_namespace
 
 
-class LinearScan:
-    """
-    Algorithm that scans over a grid on a one dimensional domain, and as output returns
-    function values at each point on grid.
-    """
+class Algorithm(ABC):
+    """Base class for a BAX Algorithm"""
 
     def __init__(self, params=None, verbose=True):
         """
@@ -28,6 +25,7 @@ class LinearScan:
         self.set_params(params)
         if verbose:
             self.print_str()
+        super().__init__()
 
     def set_params(self, params):
         """Set self.params, the parameters for the algorithm."""
@@ -35,9 +33,8 @@ class LinearScan:
 
         # Set self.params
         self.params = Namespace()
-        self.params.name = getattr(params, 'name', 'LinearScan')
-        default_x_path = [[x] for x in np.linspace(3.5, 20, 100)]
-        self.params.x_path = getattr(params, 'x_path', default_x_path)
+        self.params.name = getattr(params, "name", "LinearScan")
+        self.params.x_path = getattr(params, "x_path", [])
 
     def run_algorithm_on_f(self, f):
         """
@@ -71,18 +68,29 @@ class LinearScan:
             next_x = self.params.x_path[len_path]
         return next_x
 
-    def get_output_from_exe_path(self, exe_path):
-        """Given an execution path, return algorithm output."""
-        return exe_path.y
-
     def print_str(self):
         """Print a description string."""
-        print('*[INFO] ' + str(self))
+        print("*[INFO] " + str(self))
 
     def __str__(self):
         print_params = copy.deepcopy(self.params)
-        delattr(print_params, 'x_path')
-        return f'{self.params.name} with params={print_params}'
+        delattr(print_params, "x_path")
+        return f"{self.params.name} with params={print_params}"
+
+    @abstractmethod
+    def get_output_from_exe_path(self, exe_path):
+        pass
+
+
+class LinearScan(Algorithm):
+    """
+    Algorithm that scans over a grid on a one dimensional domain, and as output returns
+    function values at each point on grid.
+    """
+
+    def get_output_from_exe_path(self, exe_path):
+        """Given an execution path, return algorithm output."""
+        return exe_path.y
 
 
 class LinearScanRandGap(LinearScan):
@@ -122,73 +130,16 @@ class LinearScanRandGap(LinearScan):
 
         return super().run_algorithm_on_f(f)
 
-class AverageOutputs:
+
+class AverageOutputs(Algorithm):
     """
     Algorithm that computes the average of function outputs for a set of input points.
     """
-
-    def __init__(self, params=None, verbose=True):
-        """
-        Parameters
-        ----------
-        params : Namespace_or_dict
-            Namespace or dict of parameters for the algorithm.
-        verbose : bool
-            If True, print description string.
-        """
-        self.set_params(params)
-        if verbose:
-            self.print_str()
-
-    def set_params(self, params):
-        """Set self.params, the parameters for the algorithm."""
-        params = dict_to_namespace(params)
-
-        # Set self.params
-        self.params = Namespace()
-        self.params.name = getattr(params, 'name', 'AverageOutputs')
-        default_x_path = [[5.1], [5.3], [5.5], [20.1], [20.3], [20.5]]
-        self.params.x_path = getattr(params, 'x_path', default_x_path)
-
-    def run_algorithm_on_f(self, f):
-        """
-        Run the algorithm by sequentially querying function f. Return the execution path
-        and output.
-        """
-        # Initialize execution path
-        exe_path = Namespace(x=[], y=[])
-
-        # Step through algorithm
-        x = self.get_next_x(exe_path)
-        while x is not None:
-            y = f(x)
-            exe_path.x.append(x)
-            exe_path.y.append(y)
-            x = self.get_next_x(exe_path)
-
-        # Compute output from exe_path, and return both
-        output = self.get_output_from_exe_path(exe_path)
-        return exe_path, output
-
-    def get_next_x(self, exe_path):
-        """
-        Given the current execution path, return the next x in the execution path. If
-        the algorithm is complete, return None.
-        """
-        len_path = len(exe_path.x)
-        if len_path == len(self.params.x_path):
-            next_x = None
-        else:
-            next_x = self.params.x_path[len_path]
-        return next_x
 
     def get_output_from_exe_path(self, exe_path):
         """Given an execution path, return algorithm output."""
         return np.mean(exe_path.y)
 
-    def print_str(self):
-        """Print a description string."""
-        print('*[INFO] ' + str(self))
 
     def __str__(self):
         print_params = copy.deepcopy(self.params)
@@ -196,24 +147,11 @@ class AverageOutputs:
         return f'{self.params.name} with params={print_params}'
 
 
-class OptRightScan:
+class OptRightScan(Algorithm):
     """
     A simple minimization algorithm that, starting from some initial point, optimizes by
     scanning to the right until function starts to increase.
     """
-
-    def __init__(self, params=None, verbose=True):
-        """
-        Parameters
-        ----------
-        params : Namespace_or_dict
-            Namespace or dict of parameters for the algorithm.
-        verbose : bool
-            If True, print description string.
-        """
-        self.set_params(params)
-        if verbose:
-            self.print_str()
 
     def set_params(self, params):
         """Set self.params, the parameters for the algorithm."""
@@ -221,31 +159,11 @@ class OptRightScan:
 
         # Set self.params
         self.params = Namespace()
-        self.params.name = getattr(params, 'name', 'OptRightScan')
-        self.params.init_x = getattr(params, 'init_x', [4.0])
-        self.params.x_grid_gap = getattr(params, 'x_grid_gap', 0.1)
-        self.params.conv_thresh = getattr(params, 'conv_thresh', 0.2)
-        self.params.max_iter = getattr(params, 'max_iter', 100)
-
-    def run_algorithm_on_f(self, f):
-        """
-        Run the algorithm by sequentially querying function f. Return the execution path
-        and output.
-        """
-        # Initialize execution path
-        exe_path = Namespace(x=[], y=[])
-
-        # Step through algorithm
-        x = self.get_next_x(exe_path)
-        while x is not None:
-            y = f(x)
-            exe_path.x.append(x)
-            exe_path.y.append(y)
-            x = self.get_next_x(exe_path)
-
-        # Compute output from exe_path, and return both
-        output = self.get_output_from_exe_path(exe_path)
-        return exe_path, output
+        self.params.name = getattr(params, "name", "OptRightScan")
+        self.params.init_x = getattr(params, "init_x", [4.0])
+        self.params.x_grid_gap = getattr(params, "x_grid_gap", 0.1)
+        self.params.conv_thresh = getattr(params, "conv_thresh", 0.2)
+        self.params.max_iter = getattr(params, "max_iter", 100)
 
     def get_next_x(self, exe_path):
         """
@@ -259,7 +177,7 @@ class OptRightScan:
             next_x = [exe_path.x[-1][0] + self.params.x_grid_gap]
 
         if len_path >= 2:
-            conv_max_val =  np.min(exe_path.y[:-1]) + self.params.conv_thresh
+            conv_max_val = np.min(exe_path.y[:-1]) + self.params.conv_thresh
             if exe_path.y[-1] > conv_max_val:
                 next_x = None
 
@@ -272,11 +190,3 @@ class OptRightScan:
     def get_output_from_exe_path(self, exe_path):
         """Given an execution path, return algorithm output."""
         return exe_path.x[-1]
-
-    def print_str(self):
-        """Print a description string."""
-        print('*[INFO] ' + str(self))
-
-    def __str__(self):
-        print_params = copy.deepcopy(self.params)
-        return f'{self.params.name} with params={print_params}'
