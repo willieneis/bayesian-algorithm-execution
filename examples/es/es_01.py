2c4ba0dec2008a825cce6569a718672322bd413e
Willie Neiswanger <willie.neiswanger@gmail.com>
2020-12-09 03:57:45 -0500

    examples/es: update es_01 script

--- examples/es/es_01.py
+++ examples/es/es_01.py
@@ -20,8 +20,9 @@ np.random.seed(seed)
 # Set function
 f = lambda x: -1 * branin(x)
 domain = [[-5, 10], [0, 15]]
-#init_x = [7.0, 15.0]
 init_x = [4.0, 14.0]
+#init_x = [7.0, 15.0]
+#init_x = [0.0, 0.0]
 
 # Set data for model
 data = Namespace()
@@ -38,17 +39,14 @@ algo = EvolutionStrategies(
     {'n_generation': 15, 'n_population': 8, 'samp_str': 'mut', 'init_x': init_x}
 ) # Code currently requires init to 0
 
-# Pre-computed algorithm output on f:
-#algo_output_f = 10 # TODO: fix this
-
 n_iter = 40
 
 for i in range(n_iter):
     # Optimize acquisition function
-    # TODO: sample x_test here
     x_test = unif_random_sample_domain(domain, n=150)
     acqopt = AcqOptimizer(
         {'x_test': x_test, 'acq_str': 'out', 'n_path': 50, 'viz_acq': False}
+        #{'x_test': x_test, 'acq_str': 'exe', 'n_path': 50, 'viz_acq': False}
     )
     x_next = acqopt.optimize(model, algo)
 
@@ -57,7 +55,6 @@ for i in range(n_iter):
 
     # Compute current expected output
     expected_output = np.mean(acqopt.get_last_output_list(), 0)
-    #out_abs_err = np.abs(algo_output_f - expected_output)
 
     # Query function, update data
     y_next = f(x_next)
@@ -69,7 +66,6 @@ for i in range(n_iter):
     print(f'Current expected_output = {expected_output}')
     f_expected_output = f(expected_output)
     print(f'Current expected value f(expected_output) = {f_expected_output}')
-    #print(f'Current output abs. error = {out_abs_err}')
     print(f'Finished iter i = {i}')
 
     #inp = input('Press enter to continue (any other key to stop): ')
