6c6ae1cb8311037329aae635219e3f61a32a0aa0
willieneis <willie.neiswanger@gmail.com>
2021-01-02 14:06:07 -0800

    add all gpfs model code

---
+++ bax/models/gpfs/README.md
@@ -0,0 +1,1 @@
+The code in this directory is from: https://github.com/j-wilson/GPflowSampling
---
+++ bax/models/gpfs/utils/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from gpflow_sampling.utils.array_ops import *
+from gpflow_sampling.utils.linalg import *
+from gpflow_sampling.utils.gpflow_ops import *
