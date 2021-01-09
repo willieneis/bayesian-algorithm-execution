bfe17937f715d684e487d9626080a9f9fcfe44a2
willieneis <willie.neiswanger@gmail.com>
2021-01-08 20:15:25 -0800

    update AcqOptimizer with remove_x_dups

--- bax/acq/acqoptimize_new.py
+++ bax/acq/acqoptimize_new.py
@@ -38,6 +38,7 @@ class AcqOptimizer:
         self.params.opt_str = getattr(params, "opt_str", "batch")
         default_x_batch = [[x] for x in np.linspace(0.0, 40.0, 500)]
         self.params.x_batch = getattr(params, "x_batch", default_x_batch)
+        self.params.remove_x_dups = getattr(params, "remove_x_dups", False)
 
     def optimize(self, acqfunction):
         """
@@ -72,9 +73,36 @@ class AcqOptimizer:
 
     def optimize_batch(self):
         """Optimize acquisition function over self.params.x_batch."""
-        acq_list = self.acqfunction(self.params.x_batch)
-        acq_opt = self.params.x_batch[np.argmax(acq_list)]
+        x_batch = copy.deepcopy(self.params.x_batch)
+
+        # Optionally remove data.x (in acqfunction) duplicates
+        if self.params.remove_x_dups:
+            x_batch = self.remove_x_dups(x_batch)
+
+        # Optimize self.acqfunction over x_batch
+        acq_list = self.acqfunction(x_batch)
+        acq_opt = x_batch[np.argmax(acq_list)]
+
         return acq_opt
+
+    def remove_x_dups(self, x_batch):
+        """Remove elements of x_batch that are also in data.x (in self.acqfunction)"""
+
+        # NOTE this requires self.acqfunction with model.data
+        data = self.acqfunction.model.data
+
+        # NOTE this only works for data.x consisting of list-types, not for arbitrary data.x
+        for x in data.x:
+            while True:
+                try:
+                    idx, pos = next(
+                        (tup for tup in enumerate(x_batch) if all(tup[1] == x))
+                    )
+                    del x_batch[idx]
+                except:
+                    break
+
+        return x_batch
 
     def print_str(self):
         """Print a description string."""
