6c7619ffad1d2efd79e90fe9f2d15e1aa0c41265
willieneis <willie.neiswanger@gmail.com>
2021-06-22 19:26:19 -0700

    add project_to_domain util

--- bax/util/domain_util.py
+++ bax/util/domain_util.py
@@ -10,3 +10,12 @@ def unif_random_sample_domain(domain, n=1):
     list_of_arr_per_dim = [np.random.uniform(dom[0], dom[1], n) for dom in domain]
     list_of_list_per_sample = [list(l) for l in np.array(list_of_arr_per_dim).T]
     return list_of_list_per_sample
+
+
+def project_to_domain(x, domain):
+    """Project x, a list of scalars, to be within domain (a list of tuple bounds)."""
+    x_arr = np.array(x).reshape(-1)
+    min_list = [tup[0] for tup in domain]
+    max_list = [tup[1] for tup in domain]
+    x_arr_clip = np.clip(x_arr, min_list, max_list)
+    return list(x_arr_clip)
